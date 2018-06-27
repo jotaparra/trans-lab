31c49de3f0b2d7fd44b4af17a06518157457aa67
Jennifer Parra <jparra.biblio@gmail.com>
2018-06-27 15:02:58 -0400

    estructura html y css del index

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-/* Holiii acá va tu código también */
+//Para login con alert
+const go = () => { //solo funciona con este nombre y contraseña
+  if (document.form.password.value ==='laboratoria' &&  document.form.login.value ==='Valentina'){ 
+    window.open('menu.html') //abre página index si se pone correcto
+    } 
+    else{ 
+      alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
+    } 
+  }
